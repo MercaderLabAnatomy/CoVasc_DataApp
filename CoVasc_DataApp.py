5083d00e4316afa44e0f8a2611b89d9d9eafcd6a
Ernst <alexander.ernst@unibe.ch>
2022-06-08 08:42:26 +0200

    Update CoVasc_DataApp.py

--- CoVasc_DataApp.py
+++ CoVasc_DataApp.py
@@ -132,9 +132,6 @@ def plot_heatmap(df_, druglist = None, show_all = True, scale = True):
                         z= df_[df_.columns[2:]].T,
                         y= df_.columns[2:],
                         x= df_.Drug,
-                        zmin = -1,
-                        zmax = 1,
-                        zmid = 0,
                         colorscale = 'PiYG'
         ))
     else:
