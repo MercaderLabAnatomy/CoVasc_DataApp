a6564b5d3e5fe0184540fada092d0bbf782dea00
Ernst <alexander.ernst@unibe.ch>
2022-07-14 17:46:18 +0200

    Update CoVasc_DataApp.py

--- CoVasc_DataApp.py
+++ CoVasc_DataApp.py
@@ -351,10 +351,10 @@ if a_state:
         cols1 = st.columns(len(listexperiments1))
         
         for id,(col1,experiment1) in enumerate(zip(cols1,listexperiments1)):
-            st.write(experiment1)
-            st.header('Experiment ID: {}'.format( experiment1))
-        #    plot_df3 = df3.reset_index().set_index("Experiment ID").loc[experiment]
-        #    fig3 = px.line(df3.loc[x_insert].reset_index(), x="Bin [1 sec]", y="Velocity [mm/s]", color='Drug')
+            with col1:
+                st.header('Experiment ID: {}'.format( experiment1))
+                plot_df3 = df3.reset_index().set_index("Experiment ID").loc[experiment1].set_index("Drug")
+                fig3 = px.line(plot_df3.loc[x_insert].reset_index(), x="Bin [1 sec]", y="Velocity [mm/s]", color='Drug')
         #   st.plotly_chart(fig3,use_container_width=True)
     
     with st.expander("See images of larvae"):
