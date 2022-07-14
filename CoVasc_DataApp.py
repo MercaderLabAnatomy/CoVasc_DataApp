59c17d96ec4e9e353ffaf433640ddcfc0600697a
Ernst <alexander.ernst@unibe.ch>
2022-07-14 17:25:55 +0200

    Update CoVasc_DataApp.py

--- CoVasc_DataApp.py
+++ CoVasc_DataApp.py
@@ -346,9 +346,9 @@ if a_state:
         df3 = getActivityData().set_index("Drug")
         experiment_ids3 = df3.loc[x].reset_index()["Experiment ID"].astype(str).unique()
         
-        #listexperiments = {expid: df3.reset_index().set_index("Experiment ID").loc[expid]["Drug"].unique() for expid in experiment_ids3 }
+        listexperiments = {expid: df3.reset_index().set_index("Experiment ID").loc[expid]["Drug"].unique() for expid in experiment_ids3 }
         
-        st.write(type(experiment_ids3[0]))
+        st.write(listexperiments)
         
         #cols1 = st.columns(len(listexperiments))
         
