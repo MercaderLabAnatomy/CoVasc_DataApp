68bc3e44241af4dd276cde2f50df4ab3a95afff8
Ernst <alexander.ernst@unibe.ch>
2023-02-19 10:42:39 +0100

    Update CoVasc_DataApp.py

--- CoVasc_DataApp.py
+++ CoVasc_DataApp.py
@@ -136,7 +136,7 @@ def plot_heatmap(df_, druglist = None, show_all = True, scale = True):
     
     if druglist and ~show_all:
         df_ = df_.loc[druglist]
-    
+        df_["Drug__Concentration (µM)"] = df_.reset_index()["Drug"] + df_.reset_index()["Concentration (µM)"]
         
         
     if scale:
@@ -144,7 +144,7 @@ def plot_heatmap(df_, druglist = None, show_all = True, scale = True):
         fig = go.Figure(data=go.Heatmap(
                         z= df_[df_.columns[2:]].T,
                         y= df_.columns[2:],
-                        x= ['{}_{}'.format(t[0],t[1]) for t in df_.columns.tolist()],
+                        x= df_["Drug__Concentration (µM)"],
                         
                         zmid = 0,
                         colorscale = 'PiYG'
@@ -153,7 +153,7 @@ def plot_heatmap(df_, druglist = None, show_all = True, scale = True):
         fig = go.Figure(data=go.Heatmap(
                     z= df_[df_.columns[2:]].T,
                     y= df_.columns[2:],
-                    x= ['{}_{}'.format(t[0],t[1]) for t in df_.columns.tolist()],
+                    x= df_["Drug__Concentration (µM)"],
                     colorscale = 'PiYG'
         ))
     
